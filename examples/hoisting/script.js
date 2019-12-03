92a2a6aacf3b4d507f4e024f923521dc5433e8be
MD Shohanur Rahman <shohanurvu@gmail.com>
2019-12-03 21:59:28 +0600

    hoisting_add

---
+++ examples/hoisting/script.js
@@ -0,0 +1,26 @@
+
+calcalulateAge(1995);
+
+function calcalulateAge(year) {
+    console.log(2018 - year);
+}
+
+
+//retirement(1990);
+var retirement = function (year) {
+    console.log(65 - (2019 - year));
+}
+
+
+
+var age = 25;
+console.log(age);
+
+function foo() {
+
+    var age = 65;
+    console.log(age);
+}
+
+foo();
+console.log(age);
