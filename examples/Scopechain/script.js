9948e562f577b3da8d4314ed3773f7b012af3a5d
MD Shohanur Rahman <shohanurvu@gmail.com>
2019-12-03 22:21:38 +0600

    commments_added

--- examples/Scopechain/script.js
+++ examples/Scopechain/script.js
@@ -10,6 +10,10 @@ function first()
     function second()
     {
         var c='Shohan'
+        //here variable c is accesible from second funcion Scope
+        //but not b
+        //then it goes up and find it from first fucntion Scope
+        //and last the varible a is  accesible from the global Scope
         console.log(a + " " +b+" " +c);
     }
 }
