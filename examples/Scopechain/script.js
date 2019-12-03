2f37833ffe0cf74672260938ed281e02b9612805
MD Shohanur Rahman <shohanurvu@gmail.com>
2019-12-03 22:35:25 +0600

    changes

--- examples/Scopechain/script.js
+++ examples/Scopechain/script.js
@@ -1,19 +1,44 @@
-var a='hello'
+// var a='hello'
 
+// first();
+
+// function first()
+// {
+//     var b='My Name is'
+//     second();
+
+//     function second()
+//     {
+//         var c='Shohan'
+//         //here variable c is accesible from second funcion Scope
+//         //but not b
+//         //then it goes up and find it from first fucntion Scope
+//         //and last the varible a is  accesible from the global Scope
+//         console.log(a + " " +b+" " +c);
+//     }
+// }
+var a = 'Hello';
 first();
 
-function first()
-{
-    var b='My Name is'
+function first() {
+    var b = 'My Name ';
     second();
 
-    function second()
-    {
-        var c='Shohan'
-        //here variable c is accesible from second funcion Scope
-        //but not b
-        //then it goes up and find it from first fucntion Scope
-        //and last the varible a is  accesible from the global Scope
-        console.log(a + " " +b+" " +c);
+
+    function second() {
+        var c = 'is';
+        third();
     }
 }
+
+function third() {
+    
+    var d = 'Shohan'
+    console.log(a + " " + " " + d);
+}
+
+//here variable a is in Global Scope this why third
+//function can get the access 
+//d is defined in the third function in itself this ehy d 
+//is also accesible 
+//
