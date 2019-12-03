dc01bad112c92153663dc1456091e317d88eb560
MD Shohanur Rahman <shohanurvu@gmail.com>
2019-12-03 22:18:49 +0600

    scopechain_javascript

---
+++ examples/Scopechain/script.js
@@ -0,0 +1,15 @@
+var a='hello'
+
+first();
+
+function first()
+{
+    var b='My Name is'
+    second();
+
+    function second()
+    {
+        var c='Shohan'
+        console.log(a + " " +b+" " +c);
+    }
+}
