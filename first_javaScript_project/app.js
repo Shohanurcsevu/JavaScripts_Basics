bba945d15b6827aab66cc645a6c7cb31ee151641
MD Shohanur Rahman <shohanurvu@gmail.com>
2019-12-07 01:17:00 +0600

    done_dom_manipulation_without_coding_challange

--- first_javaScript_project/app.js
+++ first_javaScript_project/app.js
@@ -8,12 +8,116 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore,activePlayer,dice;
+var scores, roundScore, activePlayer, dice, gamPlaying;
 
-scores =  [0,0];
-roundScore = 0;
+function initGame() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamPlaying = true;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 
-activePlayer = 1;
-dice = Math.floor(Math.random() *6 +1);
-document.querySelector('#current-'+activePlayer).textContent = dice;
+}
+initGame();
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    if (gamPlaying) {
+        //1. Random NUmber
+        var dice = Math.floor(Math.random() * 6 + 1);
+        //2.Displa the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+
+
+        //3.Update the the result if the rolled number was not 1
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+
+
+        } else {
+            //hit the next player
+            nextPlayer();
+
+
+            //document.querySelector('.player-0-panel').classList.remove('active');
+            //document.querySelector('.player-1-panel').classList.add('active');
+
+
+        }
+
+    }
+
+
+
+});
+
+
+//document.querySelector('#current-'+activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    if(gamPlaying)
+    {
+          //add current score the global score
+    scores[activePlayer] += roundScore;
+    console.log(scores[activePlayer]);
+    //update the ui
+    //document.querySelector('.score-'+activePlayer).textContent = scores[activePlayer];
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 10) {
+       
+        document.getElementById('name-' + activePlayer).textContent = 'Winner';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        gamPlaying=false;
+
+    } else {
+        nextPlayer();
+
+    }
+
+
+    }
+  
+
+    //if player won the game
+});
+
+
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+
+
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', initGame)
